91d9237e80b2d304b1fadf6f316452e10081609e
tkx.cool <kunxin.tan@fanyou-online.com>
2024-12-22 19:44:26 +0800

    feat: update

---
+++ bin/dart_5.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: unnecessary_type_check
+
+void main() {
+  var a = 1;
+  // 判断类型
+  print(a is int);
+
+  var b = 1.0;
+  print(b is double);
+
+  var c = 'hello';
+  print(c is double);
+}
---
+++ bin/dart_6.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: unnecessary_type_check
+
+void main() {
+  // int doble 相互转换
+  final int a = 1;
+  final double b = 2.15;
+  final c = (5 / 2).toInt();
+  final d = (5 / 5).toDouble();
+
+  print(a.toDouble());
+  print(b.toInt());
+  print(c);
+  print(d);
+}
