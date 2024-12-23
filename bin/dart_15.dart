34a7778a3a027f4ccec7753cb794bd3f562864dd
tkx.cool <kunxin.tan@fanyou-online.com>
2024-12-23 10:49:52 +0800

    feat: update

---
+++ bin/dart_15.dart
@@ -0,0 +1,17 @@
+void main() {
+  int count = 0;
+  // while
+  while (count < 10) {
+    print('Count is $count');
+    count++;
+  }
+
+  print('=============================');
+
+  // do...while
+  int doCount = 0;
+  do {
+    print('doCount is $doCount');
+    doCount++;
+  } while (doCount < 10);
+}
